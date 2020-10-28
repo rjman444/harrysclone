a5ebcdea43dde0d88e79f347d2ba63f3f24dd4f1
Ryan Jansen <rjgiga444@gmail.com>
2020-10-28 21:00:05 +0800

    added header animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from "react";
-import styles from "../styles/style.module.css";
+import React, { useState } from "react";
+import styles from "../styles/header.module.css";
 
 const Header = () => {
+  const [showProduct, setProduct] = useState(false);
+
+  const showProductOnClick = () => {
+    if (!showProduct) {
+      setProduct(true);
+    } else {
+      setProduct(false);
+    }
+    console.log(showProduct);
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>HARRY'S</div>
@@ -9,20 +20,44 @@ const Header = () => {
         <a href="localhost:3000" className={styles["nav-link"]}>
           GET STARTED
         </a>
-        <a href="localhost:3000" className={styles["nav-link"]}>
+        <a
+          href="localhost:3000"
+          onClick={showProductOnClick}
+          className={styles["nav-link"]}
+        >
           PRODUCTS
         </a>
+
+        <div
+          className={`${styles["nav-products"]} ${
+            showProduct ? "" : styles["collapsed"]
+          }`}
+        >
+          <div className={styles["product-grid"]}>
+            <p>Hello</p>
+          </div>
+        </div>
+
         <a href="localhost:3000" className={styles["nav-link"]}>
           ABOUT
         </a>
-        <a href="localhost:3000" className={styles["nav-link"]}>
+        <a
+          href="localhost:3000"
+          className={`${styles["nav-link"]} ${styles.attention}`}
+        >
           SHOP BUNDLES
         </a>
       </div>
       <div className={styles["right-menu"]}>
-        <a>JOIN CORE</a>
-        <a>HELP</a>
-        <a>SIGN IN</a>
+        <a href="localhost:3000" className={styles["nav-link"]}>
+          Join Core
+        </a>
+        <a href="localhost:3000" className={styles["nav-link"]}>
+          Help
+        </a>
+        <a href="localhost:3000" className={styles["nav-link"]}>
+          Sign In
+        </a>
         <div className={styles["cart-icon"]}></div>
       </div>
     </div>
